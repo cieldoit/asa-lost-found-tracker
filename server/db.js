ea1092ff9097317723c2bd819986385b0a1f8318
cieldoit <rexilldator@gmail.com>
2026-05-11 14:03:08 +0800

    Fix DB port for Railway connection

--- server/db.js
+++ server/db.js
@@ -6,8 +6,9 @@ require('dotenv').config();
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS, // Match this to your .env
-    database: process.env.DB_NAME
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 module.exports = db;
