ededff3dcda31ea027ca0bb3a09f5473c56201d8
cieldoit <rexilldator@gmail.com>
2026-05-18 16:35:52 +0800

    Tune MySQL connection pool

--- server/db.js
+++ server/db.js
@@ -1,14 +1,19 @@
-//This is inside db.js
-
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
+  maxIdle: Number(process.env.DB_MAX_IDLE) || 10,
+  idleTimeout: Number(process.env.DB_IDLE_TIMEOUT) || 60000,
+  queueLimit: Number(process.env.DB_QUEUE_LIMIT) || 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
 module.exports = db;
